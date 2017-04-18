45c455ab42ed7c30fec7424af3a0ea658d96f6a3
Brilly Majuan <sbmajuanl@gmail.com>
2017-04-18 15:46:09 -0500

    5to desafio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var contenedor=document.getElementsByClassName("contenedor")[0];
+var rosa=document.getElemetById("rosa");
+var azul=document.getElemetById("azul");
+var verde=document.getElemetById("verde");
+var amarillo=document.getElemetById("amarillo");
+
+contenedor.style.position="relative";
+contenedor.style.background="DarkGray";
+contenedor.style.width="600px";
+contenedor.style.padding="50px";
+
+azul.style.position="absolute";
+azul.style.top="250px";
+azul.style.right="0";
